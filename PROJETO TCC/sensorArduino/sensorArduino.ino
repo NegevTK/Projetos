0aba3249020748d661af70c0aba4f1bcdac8b83e
NegevTK <luizantoniosoda@gmail.com>
2022-08-12 19:48:10 -0300

    tcc

--- PROJETO TCC/sensorArduino/sensorArduino.ino
+++ PROJETO TCC/sensorArduino/sensorArduino.ino
@@ -4,30 +4,21 @@
 #define solo_pin A6
 #define dht_type DHT11
 DHT my_dht(dht_pin, dht_type);
-
-int percent = 0;
-
+int umidade = 0;
 void setup() {
    Serial.begin(9600);
    pinMode(rele, OUTPUT);
    my_dht.begin();
 }
-
 void loop() { 
-  percent = convertToPercent(analogRead (solo_pin));
-  if(percent < 70){
+  umidade = analogRead (solo_pin);
+  int porcentagem = map(umidade, 1023, 0, 0, 100);
+  if(porcentagem < 70){
     digitalWrite(rele, HIGH);
   } else{
     digitalWrite(rele, LOW);
   }
-  String info = (String(my_dht.readTemperature()) + ":" + String(my_dht.readHumidity()) + ":" + String(percent));
+  String info = (String(my_dht.readTemperature()) + ":" + String(my_dht.readHumidity()) + ":" + String(porcentagem));
   Serial.println(info);
   delay(1000);
-}
-
-int convertToPercent(int value)
-{
-int percentValue = 0;
-percentValue = map(value, 1023, 465, 0, 100);
-return percentValue;
 }
